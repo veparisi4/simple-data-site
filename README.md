534242991f0504c9cef0acd8837d7fa71d415b99
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-04 15:31:06 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Data Website
+
+This website serves as a simple opportunity for users to build an online
+gallery loading a dataset from a CSV and iterating over it with Vue.
---
+++ script.js
@@ -0,0 +1,15 @@
+d3.csv("./favorite-songs.csv").then(function(songs) {
+  /*
+  const app = new Vue({
+    // This designates the el(ement) in which Vue will operate.
+    // "#" is short hand for "the element with the id of…"
+    el: "#main-container",
+    // This designates the data Vue will send to our page. 
+    // For example, it defines a "card" property/variable.
+    data: {
+      card: "card made by Vue!",
+      songs: songs
+    }
+  }); 
+  */
+});
