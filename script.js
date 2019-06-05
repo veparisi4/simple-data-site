04a53b57d3bff2959c7b0ad2debc96569b62a950
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-05 11:30:26 -0400

    Add stylesheet and simplify js.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-d3.csv("./favorite-songs.csv").then(function(songs) {
-  /*
-  const app = new Vue({
-    // This designates the el(ement) in which Vue will operate.
-    // "#" is short hand for "the element with the id of…"
-    el: "#main-container",
-    // This designates the data Vue will send to our page. 
-    // For example, it defines a "card" property/variable.
-    data: {
-      card: "card made by Vue!",
-      songs: songs
-    }
-  }); 
-  */
-});
+/*
+const app = new Vue({
+  // This designates the el(ement) in which Vue will operate.
+  // "#" is short hand for "the element with the id of…"
+  el: "#main-container",
+  // This designates the data Vue will send to our page. 
+  // For example, it defines a "card" property/variable.
+  data: {
+    card: "card made by Vue!"
+  }
+}); 
+*/
