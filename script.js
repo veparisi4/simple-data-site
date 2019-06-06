c41a759b34c4b7b33568434f3c50e2cda5ae8186
Valentina Parisi <valentinaparisi1@dyn-129-236-180-216.dyn.columbia.edu>
2019-06-06 12:50:20 -0400

    update favoriate songs

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-/*
-const app = new Vue({
-  // This designates the el(ement) in which Vue will operate.
-  // "#" is short hand for "the element with the id of…"
-  el: "#main-container",
-  // This designates the data Vue will send to our page. 
-  // For example, it defines a "card" property/variable.
-  data: {
-    card: "card made by Vue!"
-  }
-}); 
-*/
+
+const dataUrl = "https://plain-plain-text.github.io/simple-data-site/favorite-songs.csv";
+d3.csv(dataUrl).then(function(songs) {
+  // print the value of the songs parameter
+  console.log(songs);
+  const app = new Vue({
+    el: "#main-container",
+    data: {
+      card: "card made by Vue!",
+      songs: songs
+    }
+  });
+});
