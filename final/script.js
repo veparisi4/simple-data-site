ddd9b74ace38617343a5bb021209f91f8c08f9dd
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-06-05 23:55:29 -0400

    Remove commented out d3 invocation

--- final/script.js
+++ final/script.js
@@ -1,6 +1,5 @@
 const dataUrl = "https://plain-plain-text.github.io/simple-data-site/favorite-songs.csv";
 
-// d3.csv(`http://cors.io/?${dataUrl}`).then(function(songs) {
 d3.csv(dataUrl).then(function(songs) {
   const app = new Vue({
     // This designates the el(ement) in which Vue will operate.
